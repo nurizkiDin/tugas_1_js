544a00c76c988e0828e4146c7fd50ccac4b23c48
Nurizki <nutrisi1udin@gmail.com>
2022-10-02 19:21:27 +0700

    tugas 1 js
    tugas niomic

---
+++ tugas1.js
@@ -0,0 +1,17 @@
+var nama_depan = "Depan";
+let nama_belakang = "Belakang";
+const umur = 22;
+
+nama_depan = "First";
+nama_belakang = "Islamanuddin";
+//umur = 2; error allert
+
+if(true){
+    var nama_depan = "Nurizki";
+    let nama_belakang = "Last";
+    const umur = null;
+}
+
+console.log(nama_depan);
+console.log(nama_belakang);
+console.log(umur);
